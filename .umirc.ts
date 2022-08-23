547f68b56c85f127e2705589c14afa36e4b7bc31
tangjiahui <tjhaaa@163.com>
2022-08-23 16:16:12 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'umi';
+
+// https://v3.umijs.org/zh-CN/config
+export default defineConfig({
+  fastRefresh: {},
+  nodeModulesTransform: {
+    type: 'none',
+  },
+});
